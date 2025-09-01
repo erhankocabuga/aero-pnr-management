9331ba68da2db53fef47f754720037bfad56ce0f
Erhan Kocabuga <erhankocabuga@gmail.com>
2025-09-01 09:18:32 +0300

    chore: add initial Spring Boot project

---
+++ src/main/java/com/skylineair/aeropnr/AeroPnrManagementApplication.java
@@ -0,0 +1,13 @@
+package com.skylineair.aeropnr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroPnrManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroPnrManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skylineair/aeropnr/AeroPnrManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skylineair.aeropnr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeroPnrManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
